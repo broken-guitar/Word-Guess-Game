a79054a10f40e7efae5974cbe15fc0ec0557b389
David Flores <drfloresj@gmail.com>
2020-02-09 01:05:45 -0800

    added word marquee rending and logic to check guess with word letters; progress ~25%.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,8 @@
 var arrWordMasterList = ["apple", "banana", "orange", "grape", "strawberry"];
 var arrWordPlayList = arrWordMasterList.slice(); // copy the word list into a second array for slicing
 var strWord;
-var arrLetters = [];
-var arrLettersGuessed = [];
+var arrLetterElements = [];
+var strLettersGuessed;
 var score = 0;
 var strGuess;
 
@@ -13,7 +13,7 @@ var htmlGuess = document.getElementById("user-guess");
 var htmlWordMarquee = document.getElementById("word-marquee");
 console.log(htmlWordMarquee);
 
-// MAIN PROCESS 
+// MAIN PROCESS
 // ###############################
 
 // initialize page
@@ -23,60 +23,54 @@ var randomIndex = Math.floor(Math.random() * arrWordPlayList.length); // randoml
 console.log(randomIndex);
 
 strWord = arrWordPlayList[randomIndex]; // get string value from array with random index
-console.log(strWord)
+console.log(strWord);
 
-// show word letter placeholders
-for (i=0; i<strWord.length;i++){
-    var letterDiv = document.createElement("div");
-    letterDiv.setAttribute("id", strWord.charAt(i));
-    letterDiv.setAttribute("class", "word-marquee-letters")
-    letterDiv.textContent = strWord[i];
-    console.log(letterDiv);
+// render word marquee
+for (i = 0; i < strWord.length; i++) {
+  var letterDiv = document.createElement("div");
+  letterDiv.setAttribute("id", i);
+  letterDiv.setAttribute("letter-value-data", strWord.charAt(i));
+  letterDiv.setAttribute("letter-guessed-data", false);
+  letterDiv.setAttribute("class", "word-marquee-letters");
+  letterDiv.textContent = "_";
 
-    var test = document.getElementById("word-marquee");
-    console.log(htmlWordMarquee);
-    htmlWordMarquee.appendChild(letterDiv);
+  arrLetterElements.push(letterDiv);
+  //   console.log("arrayLetters" + i + ".id = " + arrLetters[i].getAttribute("id"));
+
+  htmlWordMarquee.appendChild(letterDiv);
 }
-// get letter guess input from user
-document.onkeyup = function (event) {
-    strGuess = event.key;
-    console.log(strGuess);
-    // htmlGuess.textContent = strGuess;
-    document.getElementById("user-guess").textContent = strGuess;
 
+// get letter guess input from user
+document.onkeyup = function(event) {
+  strGuess = event.key;
+  document.getElementById("user-guess").textContent = strGuess;
+
+  // use regex to see if key press is alpha
+  if (/[a-z]/i) {
+    // key pressed was an alpha, now check the guess against each letter in word
+    arrLetterElements.forEach(function(current, index, arr) {
+      var letter = current.getAttribute("letter-value-data");
+      if (letter.toLowerCase() === strGuess.toLowerCase()) {
+        current.textContent = letter;
+        current.setAttribute("letter-guessed-data", true);
+      }
+      //   console.log("currentLetter = " + currentLetter.getAttribute("letter-value-data"));
+    });
+  } else {
+    // key pressed wasn't an alpha
+  }
 };
 
-
-
-
-
-
-// FUNCTIONS 
+// FUNCTIONS
 // ###############################
 
 // function to render word platform
 
-function renderMarquee() {
-
-
-};
-
+function renderMarquee() {}
 
 // example code ------------------------------------------------
 
 // If the first character in the current animal is "c" or "o", alert the following message.
-if (myFarm[j].charAt(0) === "c" || myFarm[j].charAt(0) === "o") {
-    alert("Starts with a c or an o!");
-}
-
-document.onkeyup = function (event) {
-    userText.textContent = event.key;
-};
-
-var userText = document.getElementById("user-text");
-
-// Next, we give JavaScript a function to execute when onkeyup event fires.
-// Let's start by grabbing a reference to the <span> below.
-document.onkeyup = function (event) {
-    userText.textContent = event.key;
-}
+// if (myFarm[j].charAt(0) === "c" || myFarm[j].charAt(0) === "o") {
+//   alert("Starts with a c or an o!");
+// }
