352b2b150e17c93d0189f31993caf35c27dfba35
David Flores <drfloresj@gmail.com>
2020-02-11 00:23:31 -0800

    final commit to meet min requirements.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var strWord;
 
 var arrLetterObjects = [];
 var arrLetterElements = [];
-var strLettersGuessed
+var strLettersGuessed;
 
 var gameOver = false;
 var wordOver = false;
@@ -23,131 +23,131 @@ var htmlLettersGuessed = document.getElementById("letters-guessed-p");
 var htmlUserScore = document.getElementById("user-score-p");
 var htmlGuessesLeft = document.getElementById("guesses-left-p");
 
+main();
 
+function main() {
+  // initialize
+  strWord = "";
+  htmlUserScore.textContent = "Score: " + userScore;
+  arrWordPlayList = arrWordMasterList.slice(); // copy the word list into a second array for splicing (to prevent word replays)
+  htmlGameBanner.innerHTML = "<p id='continue'>Press any key to start!</p>";
 
+  selectNewWord();
 
-
-// initialize
-strWord = "";
-htmlUserScore.textContent = "Score: " + userScore;
-arrWordPlayList = arrWordMasterList.slice(); // copy the word list into a second array for splicing (to prevent word replays)
-selectNewWord();
-
-
-
-// get guess input from user
-document.onkeyup = function (event) {
-  strGuess = event.key;
-
-  // clear game banner if user is playing
-  if (wordOver === false) {
-    htmlGameBanner.innerHTML = ""
-    userGuessSuccess = false;
-  }
-  
-
-  // use regex to check if key pressed is a (single) letter key
-  if (/^[a-z]{1}$/i.test(strGuess)) {
-
-    // key press is a letter
-
-    // check if letter is already guessed
-    if (strLettersGuessed.indexOf(strGuess.toUpperCase()) === -1) {
-
-      // letter was not guessed yet
-
-      // render guess on page
-      htmlGuess.textContent = strGuess;
-
-      //save guess, sort alpha (prevent spelling bad words!); and render
-      strLettersGuessed += strGuess.toUpperCase();
-      htmlLettersGuessed.textContent = sortAlphabetically(strLettersGuessed);
-
-      // check guess against each letter in word
-      arrLetterObjects.forEach(function (current, index, arr) {
-        if (current.letter.toLowerCase() === strGuess.toLowerCase()) {
-
-          // LETTER GUESSED CORRECTLY
-
-          // record the letter as guessed
-          current.guessed = true;
-          userGuessSuccess = true;
-          // show the correctly guessed letter(s) on the marquee
-          document.getElementById(current.wordIndex).textContent = current.letter;
-
-          // if every letter has been guessed correctly...
-          if (
-            arrLetterObjects.every(function (element) {
-              return element.guessed === true;
-            })
-          ) {
-            // ...then word is complete, update user score
-            userScore += 1;
-            htmlUserScore.textContent = "Score: " + userScore;
-            htmlGameBanner.innerHTML = "You guessed the word <strong>" + strWord + "</strong>! <p id='continue'>Press any key to continue</p>"
-
-            setTimeout(function () {
-
-            }, 500);
-
-            // select a new word
-            selectNewWord();
-
-          } else {
-            // word is not complete
-          }
-        } else {
-          // letter was NOT guessed correctly
-        }
-      });
-      if (userGuessSuccess === false){
-        console.log('minus');
-        guessesLeft -= 1;
-        htmlGuessesLeft.textContent = "Guesses left: " + guessesLeft;
-        if (guessesLeft < 1){
-          wordOver = true;
-          htmlGameBanner.innerHTML = "You ran out of guesses! <p id='continue'>Press any key to continue</p>"
-          selectNewWord();
-         
-        } 
-      }
-      
-
-    } else {
-      console.log("already guessed '" + strGuess + "'"); // ??? do something if letter was already guessed?
+  // get guess input from user
+  document.onkeyup = function(event) {
+    strGuess = event.key;
+    if (gameOver) {
+      return;
     }
-  } else {
-    // key press is not alpha
-    console.log("regex false; key= " + strGuess);
-  }
+    // clear game banner if user is playing; reset user guess successful var
+    if (wordOver === false) {
+      htmlGameBanner.innerHTML = "";
+      userGuessSuccess = false;
+    }
+
+    // use regex to check if key pressed is a (single) letter key
+    if (/^[a-z]{1}$/i.test(strGuess)) {
+      // key press is a letter
+
+      // check if letter is already guessed
+      if (strLettersGuessed.indexOf(strGuess.toUpperCase()) === -1) {
+        // letter was not guessed yet
+
+        // render guess on page
+        htmlGuess.textContent = strGuess;
+
+        //save guess, sort alpha (prevent spelling bad words!); and render
+        strLettersGuessed += strGuess.toUpperCase();
+        htmlLettersGuessed.textContent = sortAlphabetically(strLettersGuessed);
+
+        // check guess against each letter in word
+        arrLetterObjects.forEach(function(current, index, arr) {
+          if (current.letter.toLowerCase() === strGuess.toLowerCase()) {
+            // LETTER GUESSED CORRECTLY
+
+            // record the letter as guessed
+            current.guessed = true;
+            userGuessSuccess = true;
+            // show the correctly guessed letter(s) on the marquee
+            document.getElementById(current.wordIndex).textContent =
+              current.letter;
+
+            // if every letter has been guessed correctly...
+            if (
+              arrLetterObjects.every(function(element) {
+                return element.guessed === true;
+              })
+            ) {
+              // ...then word is complete, update user score
+              userScore += 1;
+              htmlUserScore.textContent = "Score: " + userScore;
+              htmlGameBanner.innerHTML =
+                "You guessed the word <strong>" +
+                strWord +
+                "</strong>! <p id='continue'>Press any key to continue</p>";
+
+              setTimeout(function() {}, 500);
+
+              // select a new word
+              if (selectNewWord()) {
+                return;
+              }
+            }
+          } else {
+            // letter was NOT guessed correctly
+          }
+        });
+        if (userGuessSuccess === false) {
+          guessesLeft -= 1;
+          htmlGuessesLeft.textContent = "Guesses left: " + guessesLeft;
+          if (guessesLeft < 1) {
+            wordOver = true;
+            htmlGameBanner.innerHTML =
+              "You ran out of guesses! <p id='continue'>Press any key to continue</p>";
+            if (selectNewWord()) {
+              return;
+            }
+          }
+        }
+      } else {
+        console.log("already guessed '" + strGuess + "'"); // ??? do something if letter was already guessed?
+      }
+    } else {
+      // key press is not alpha
+      console.log("regex false; key= " + strGuess);
+    }
+  };
 }
-
-
 
 // FUNCTIONS
 // ###############################
 
 // selects a new word and updates page
 function selectNewWord() {
-
   // check if all words played
-  if (arrWordPlayList.length < 1){
-    alert("Game Over!")
-
+  if (arrWordPlayList.length < 2 || gameOver) {
+    console.log("game over: " + gameOver);
+    gameOver = true;
+    htmlGameBanner.textContent = "Game Over!";
+    htmlWordMarquee.innerHTML = "";
+    htmlGuess.innerHTML = "";
+    htmlLettersGuessed.innerHTML = "";
+    return gameOver;
   }
 
   //reset vars and elements
   userGuessSuccess = false;
-  wordOver = false; 
+  wordOver = false;
   guessesLeft = 0;
   strLettersGuessed = "";
   arrLetterObjects.splice(0, arrLetterObjects.length);
   arrLetterElements.splice(0, arrLetterElements.length);
-  htmlWordMarquee.innerHTML = '';
-  htmlGuess.innerHTML = '';
-  htmlLettersGuessed.innerHTML = '';
+  htmlWordMarquee.innerHTML = "";
+  htmlGuess.innerHTML = "";
+  htmlLettersGuessed.innerHTML = "";
 
-  // randomly select a word that hasn't been played yet  
+  // randomly select a word that hasn't been played yet
   if (strWord === "") {
     // first word of the game
     var randomIndex = Math.floor(Math.random() * arrWordPlayList.length); // randomly select an index of words-in-play array
@@ -162,18 +162,17 @@ function selectNewWord() {
   console.log(strWord);
 
   guessesLeft = uniqueLetterCount(strWord);
-  htmlGuessesLeft.textContent = "Guesses left: " + guessesLeft
+  htmlGuessesLeft.textContent = "Guesses left: " + guessesLeft;
 
   // initialize word marquee; loop through each letter in the word
   for (i = 0; i < strWord.length; i++) {
-    
     // create letter object to store info about the  letter in the word
     var objLetter = {
       wordIndex: i,
       letter: strWord.charAt(i),
       guessed: false
     };
-    
+
     // push to an array to access later
     arrLetterObjects.push(objLetter);
 
@@ -189,18 +188,18 @@ function selectNewWord() {
 }
 
 function sortAlphabetically(string) {
-  var arrSplit = string.split('');
+  var arrSplit = string.split("");
   var arrSort = arrSplit.sort();
-  var arrJoin = arrSort.join('');
+  var arrJoin = arrSort.join("");
   return arrJoin;
 }
 
 // return number of unique letters in a word
-function uniqueLetterCount(string){
+function uniqueLetterCount(string) {
   var strTemp = string;
   var uniques = "";
-  for (var i = 0; i < strTemp.length; i++){
-    if (uniques.indexOf(strTemp[i]) == -1){
+  for (var i = 0; i < strTemp.length; i++) {
+    if (uniques.indexOf(strTemp[i]) == -1) {
       uniques += strTemp[i];
     }
   }
