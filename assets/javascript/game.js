8c00aeba56fea32ec66e72204e0cd626633aec32
DFlores <drfloresj@gmail.com>
2020-02-10 17:02:16 -0800

    added working logic and code to iterate through guessed words; progress 70%.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,118 +2,156 @@
 // ###############################
 
 var arrWordMasterList = ["apple", "banana", "orange", "grape", "strawberry"];
-var arrWordPlayList = arrWordMasterList.slice(); // copy the word list into a second array for slicing
+var arrWordPlayList = [];
 var strWord;
 
-var objLetter = {};
 var arrLetterObjects = [];
 var arrLetterElements = [];
-var strLettersGuessed = "";
+var strLettersGuessed
 
+var gameRun = true;
+var wordRun = true;
 var score = 0;
 var strGuess;
 
-var htmlGuess = document.getElementById("user-guess");
 var htmlWordMarquee = document.getElementById("word-marquee");
-console.log(htmlWordMarquee);
+var htmlGuess = document.getElementById("user-guess");
+var htmlLettersGuessed = document.getElementById("letters-guessed-p");
 
-// MAIN PROCESS
-// ###############################
 
-// initialize page
 
-// randomly select a word that hasn't been played yet
-var randomIndex = Math.floor(Math.random() * arrWordPlayList.length); // randomly select an index of words-in-play array
-console.log(randomIndex);
 
-strWord = arrWordPlayList[randomIndex]; // get string value from array with random index
-console.log(strWord);
 
-// initialize word marquee; loop through each letter in the word
-for (i = 0; i < strWord.length; i++) {
-  // create letter object to store info about the letter in the word,
-  // then store push to an array to access later
-  var objLetter = {
-    wordIndex: i,
-    letter: strWord.charAt(i),
-    guessed: false
-  };
-  arrLetterObjects.push(objLetter);
+// initialize
+strWord = "";
+arrWordPlayList = arrWordMasterList.slice(); // copy the word list into a second array for splicing (to prevent word replays)
+selectWord();
 
-  // create an html element for the letter
-  var letterDiv = document.createElement("div");
-  letterDiv.setAttribute("id", i);
-  letterDiv.setAttribute("class", "word-marquee-letters");
-  letterDiv.textContent = "_";
 
-  // render the letter element on the page
-  htmlWordMarquee.appendChild(letterDiv);
-}
 
 // get letter guess input from user
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
   strGuess = event.key;
 
   // use regex to check if key pressed is a (single) letter key
   if (/^[a-z]{1}$/i.test(strGuess)) {
+
     // key press is a letter
-    console.log("regex true; key = " + strGuess);
-    document.getElementById("user-guess").textContent = strGuess; // render guess on page
-    strLettersGuessed += strGuess;
-    document.getElementById(
-      "letters-guessed-p"
-    ).textContent = strLettersGuessed;
-    // check guess against each letter in word
-    arrLetterObjects.forEach(function(current, index, arr) {
-      if (current.letter.toLowerCase() === strGuess.toLowerCase()) {
-        // letter was guessed correctly
 
-        // record the letter as guessed
-        current.guessed = true;
+    // check if letter is already guessed
+    if (strLettersGuessed.indexOf(strGuess) === -1) {
 
-        // show the correctly guessed letter(s) on the marquee
-        document.getElementById(current.wordIndex).textContent = current.letter;
+      console.log("letter '" + strGuess + "' was not guessed yet");
 
-        // if every letter has been guessed correctly
-        if (
-          arrLetterObjects.every(function(element) {
-            return element.guessed === true;
-          })
-        ) {
-          // word is complete: update user score
-          // <need code>
-          setTimeout(function() {
-            alert('You guessed the word "' + strWord + '"!');
-          }, 1000);
+      // render guess on page
+      htmlGuess.textContent = strGuess;
 
-          // <need code>
-          // if not all words played, move to next word...
-          // re-initialize page with a new word
+      //save guess, sort alpha (prevent spelling bad words!), and render
+      strLettersGuessed += strGuess;
+      htmlLettersGuessed.textContent = sortAlphabetically(strLettersGuessed);
+
+      // check guess against each letter in word
+      arrLetterObjects.forEach(function (current, index, arr) {
+        if (current.letter.toLowerCase() === strGuess.toLowerCase()) {
+
+          // record the letter as guessed
+          current.guessed = true;
+
+          // show the correctly guessed letter(s) on the marquee
+          document.getElementById(current.wordIndex).textContent = current.letter;
+
+          // if every letter has been guessed correctly...
+          if (
+            arrLetterObjects.every(function (element) {
+              return element.guessed === true;
+            })
+          ) {
+            // ...then word is complete, update user score
+            // <need code>
+
+            setTimeout(function () {
+              alert('You guessed the word "' + strWord + '"!');
+            }, 500);
+
+            // select a new word
+            selectWord();
+
+          } else {
+            // word is not complete
+            // <need code> tell loop to continue
+          }
         } else {
-          // word is not complete
-          // <need code> tell loop to continue
+          // letter was NOT guessed correctly
+          // add letter to letters-guessed content
         }
-      } else {
-        // letter was NOT guessed correctly
-        // add letter to letters-guessed content
-      }
-    });
+      });
+    } else {
+      console.log("already guessed '" + strGuess + "'"); // ??? do something if letter was already guessed?
+    }
   } else {
     // key press is not alpha
     console.log("regex false; key= " + strGuess);
   }
-};
+}
+
+
 
 // FUNCTIONS
 // ###############################
 
-// function to render word platform
+// select new word and render page
+function selectWord() {
 
-function renderMarquee() {}
+  //clear elements and variables
+  strLettersGuessed = "";
+  arrLetterObjects.splice(0, arrLetterObjects.length);
+  arrLetterElements.splice(0, arrLetterElements.length);
+  htmlWordMarquee.innerHTML = '';
+  htmlGuess.innerHTML = '';
+  htmlLettersGuessed.innerHTML = '';
 
-// example code ------------------------------------------------
 
-// If the first character in the current animal is "c" or "o", alert the following message.
-// if (myFarm[j].charAt(0) === "c" || myFarm[j].charAt(0) === "o") {
-//   alert("Starts with a c or an o!");
-// }
+
+  // randomly select a word that hasn't been played yet  
+  if (strWord === "") {
+    // first word of the game
+    var randomIndex = Math.floor(Math.random() * arrWordPlayList.length); // randomly select an index of words-in-play array
+  } else {
+    // remove the last played word, then randomly select the next word
+    var lastWordIndex = arrWordPlayList.indexOf(strWord);
+    arrWordPlayList.splice(lastWordIndex, 1);
+    var randomIndex = Math.floor(Math.random() * arrWordPlayList.length); // randomly select an index of words-in-play array
+  }
+
+  strWord = arrWordPlayList[randomIndex]; // get string value from array with random index
+  console.log(strWord);
+
+  // initialize word marquee; loop through each letter in the word
+  for (i = 0; i < strWord.length; i++) {
+    // create letter object to store info about the letter in the word,
+    // then store push to an array to access later
+    var objLetter = {
+      wordIndex: i,
+      letter: strWord.charAt(i),
+      guessed: false
+    };
+
+    arrLetterObjects.push(objLetter);
+
+    // create an html element for the letter
+    var letterDiv = document.createElement("div");
+    letterDiv.setAttribute("id", i);
+    letterDiv.setAttribute("class", "word-marquee-letters");
+    letterDiv.textContent = "_";
+
+    // render the letter element on the page
+    htmlWordMarquee.appendChild(letterDiv);
+  }
+}
+
+function sortAlphabetically(str) {
+  var arrSplit = str.split('');
+  var arrSort = arrSplit.sort();
+  var arrJoin = arrSort.join('');
+  return arrJoin;
+}
